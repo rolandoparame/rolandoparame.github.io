5d34764311c6cc7111b48a5148fe07b0a1b36c8d
r0llie <59386873+r0llie@users.noreply.github.com>
2020-02-16 18:08:01 +1000

    commit on 'master'

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,3 +1,16 @@
+// Fade in on load
 $('#particles-js').delay(2000).animate({'opacity':'1'},2500);
-$('.text').delay(2000).animate({'opacity':'1'},2500);
-$('.page-link').delay(2000).animate({'opacity':'1'},2500);
+$('.subtitle').delay(2000).animate({'opacity':'1'},2500);
+$('#portfolioBtn').delay(2000).animate({'opacity':'1'},2500);
+
+// // on hover of nav link, transition 0.5sec font color to blue using .highlight
+// $('.nav-item').click(function(){
+//     // $(this).
+// });
+
+
+// //on click of nav link, keep selected nav link heading in navbar color blue
+// $('.nav-item').click(function(){
+//     alert("clicked nav");
+// });
+
