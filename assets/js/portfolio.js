6d3ccbf264e8ec11a3b89c193240b98ec5c7dd8c
Rollie <59386873+rolandoparame@users.noreply.github.com>
2020-10-21 23:14:42 +1000

    Add files via upload

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,7 +1,7 @@
 // Fade in on load
-$('#particles-js').delay(2000).animate({'opacity':'1'},2500);
-$('.subtitle').delay(2000).animate({'opacity':'1'},2500);
-$('#portfolioBtn').delay(2000).animate({'opacity':'1'},2500);
+$('#particles-js').delay(0).animate({'opacity':'1'},1500);
+// $('.subtitle').delay(2000).animate({'opacity':'1'},2500);
+// $('#portfolioBtn').delay(2000).animate({'opacity':'1'},2500);
 
 // // on hover of nav link, transition 0.5sec font color to blue using .highlight
 // $('.nav-item').click(function(){
