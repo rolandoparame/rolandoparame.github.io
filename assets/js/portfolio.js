bdddb5bc52d67d23f873312cc28da1e0738c4395
Rollie <59386873+rolandoparame@users.noreply.github.com>
2020-10-25 14:05:23 +1000

    Add files via upload

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,5 +1,6 @@
 // Fade in on load
 $('#particles-js').delay(0).animate({'opacity':'1'},1500);
+// $('.typewriter-second').delay(0).animate({'opacity':'1'},1500);
 // $('.subtitle').delay(2000).animate({'opacity':'1'},2500);
 // $('#portfolioBtn').delay(2000).animate({'opacity':'1'},2500);
 
@@ -14,3 +15,23 @@ $('#particles-js').delay(0).animate({'opacity':'1'},1500);
 //     alert("clicked nav");
 // });
 
+$(document).ready(function(){
+    var view = {};
+    
+    var checkPosition = function(){
+       var elem = $('.fadein'), top = elem.offset().top;
+    
+                    if (top - view.limit < 300) {
+                        elem.css('display','none'); 
+                    }
+                };
+    
+           $(window).bind('scroll', function() {
+    
+                view.top = $(window).scrollTop();
+                view.limit = view.top + $(window).height();
+    
+                checkPosition();
+            }); 
+    
+    });
